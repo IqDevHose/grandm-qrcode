4a2ee5041c76cdeeba198330913e5deeecd350bc
Nooralhussaien Almalki <noor.hussein@iqdevhouse.com>
2024-09-02 15:59:39 +0300

    fixed bugs and design flows

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import ItemDetailSheet from "./components/ItemDetailSheet"; // Import the new component
 import { Item, Category } from "./lib/types";
 
-const resId = 'clztug1a60000hb92oth3lmxp';
+const resId = "clztug1a60000hb92oth3lmxp";
 
 const App: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>("All");
@@ -14,29 +14,38 @@ const App: React.FC = () => {
   const [selectedItem, setSelectedItem] = useState<Item | null>(null); // state to track the selected item
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredItems, setFilteredItems] = useState<Item[]>([]);
-  const [groupedItems, setGroupedItems] = useState<{ [key: string]: Item[] }>({}); // State to track grouped items
+  const [groupedItems, setGroupedItems] = useState<{ [key: string]: Item[] }>(
+    {}
+  ); // State to track grouped items
 
   const query = useQuery({
-    queryKey: ['restaurant', resId],
+    queryKey: ["restaurant", resId],
     queryFn: async () => {
       const res = await axios.get(`http://localhost:3000/restaurant/${resId}`);
       return res.data;
     },
   });
 
+  const themeColor = query?.data?.theme?.primary;
+
   useEffect(() => {
     // Initialize items based on activeTab and group items by category
     if (activeTab === "All") {
       const allItems =
-        query?.data?.categories?.flatMap((category: Category) => category.items) || [];
+        query?.data?.categories?.flatMap(
+          (category: Category) => category.items
+        ) || [];
       setItems(allItems);
       setFilteredItems(allItems); // Initialize filteredItems with all items
 
       // Group items by category
-      const grouped = query?.data?.categories?.reduce((acc: { [key: string]: Item[] }, category: Category) => {
-        acc[category.name] = category.items;
-        return acc;
-      }, {});
+      const grouped = query?.data?.categories?.reduce(
+        (acc: { [key: string]: Item[] }, category: Category) => {
+          acc[category.name] = category.items;
+          return acc;
+        },
+        {}
+      );
       setGroupedItems(grouped || {});
     } else {
       const selectedCategory = query?.data?.categories?.find(
@@ -63,38 +72,46 @@ const App: React.FC = () => {
 
   return (
     <>
-    <div className="md:px-72 px-2 py-10 w-full h-screen overflow-hidden bg-slate-100">
+      <div className="md:px-72 px-2 py-10 w-full h-screen overflow-hidden bg-slate-100">
         {/* search bar and category tabs */}
-        <div className="sticky top-0  z-10 ">
+        <div className="sticky top-0 z-10 ">
           <div className="flex gap-3 items-center relative rounded-full md:rounded">
-
             <Input
               placeholder="Search here..."
               className="rounded-full border-none w-full"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)} // Update searchQuery state on input change
             />
-            <Search className="absolute right-5 text-gray-500" />
+            <Search className="absolute right-5" />
           </div>
           {/* categories */}
-          <div className="flex space-x-4 p-4 overflow-x-auto border-t border-b mt-4 bg-white">
+          <div className="flex mx-2 rounded-lg space-x-4 p-4 overflow-x-scroll mt-10 bg-white">
             <button
               onClick={() => setActiveTab("All")}
-              className={`${activeTab === "All"
-                ? "bg-gray-900 text-white"
-                : "bg-gray-200 text-gray-900"
-                } whitespace-nowrap px-4 py-2 rounded-full text-sm font-medium focus:outline-none transition-colors duration-200`}
+              className={`whitespace-nowrap px-4 py-2 rounded-full text-sm font-medium focus:outline-none transition-colors duration-200 ${
+                activeTab === "All" ? "text-white" : "bg-gray-200 text-gray-900"
+              }`}
+              style={{
+                backgroundColor:
+                  activeTab === "All" ? query?.data?.theme?.primary : "",
+              }}
             >
               All
             </button>
+
             {query?.data?.categories?.map((tab: Category) => (
               <button
                 key={tab.id}
                 onClick={() => setActiveTab(tab.id)}
-                className={`${activeTab === tab.id
-                  ? "bg-gray-900 text-white"
-                  : "bg-gray-200 text-gray-900"
-                  } whitespace-nowrap px-4 py-2 rounded-full text-sm font-medium focus:outline-none transition-colors duration-200`}
+                className={`whitespace-nowrap px-4 py-2 rounded-full text-sm font-medium focus:outline-none transition-colors duration-200 ${
+                  activeTab === tab.id
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-900 "
+                }`}
+                style={{
+                  backgroundColor:
+                    activeTab === tab.id ? query?.data?.theme?.primary : "",
+                }}
               >
                 {tab.name}
               </button>
@@ -102,11 +119,12 @@ const App: React.FC = () => {
           </div>
         </div>
         {/* menu */}
-        <div className="mt-10 overflow-y-auto h-[calc(100vh-250px)] px-4 pb-20">
+        <div className="overflow-y-auto h-[calc(100vh-250px)] px-4 pb-20 ">
           {searchQuery.trim() ? (
             filteredItems.length > 0 ? (
               filteredItems.map((item) => (
                 <ItemDetailSheet
+                  themeColor={themeColor}
                   key={item.id}
                   item={item}
                   setSelectedItem={setSelectedItem}
@@ -116,33 +134,40 @@ const App: React.FC = () => {
               <p className="text-center text-gray-500">No items found.</p>
             )
           ) : activeTab === "All" ? (
-            Object.keys(groupedItems).map((categoryName) => (
-              <div key={categoryName} className="pb-4">
-                <h2 className="text-xl font-bold text-gray-800 py-4">{categoryName}</h2>
-                <div className="space-y-4">
-                  {groupedItems[categoryName].map((item) => (
-                    <ItemDetailSheet
-                      key={item.id}
-                      item={item}
-                      setSelectedItem={setSelectedItem}
-                    />
-                  ))}
+            <div className=" overflow-y-scroll h-full ">
+              {Object.keys(groupedItems).map((categoryName) => (
+                <div key={categoryName} className="mt-7">
+                  <h2 className="text-xl font-bold text-gray-800 px-4 mb-2">
+                    {categoryName}
+                  </h2>
+                  <div className="space-y-4">
+                    {groupedItems[categoryName].map((item) => (
+                      <ItemDetailSheet
+                        themeColor={themeColor}
+                        key={item.id}
+                        item={item}
+                        setSelectedItem={setSelectedItem}
+                      />
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))
+              ))}
+            </div>
           ) : (
-            filteredItems.map((item) => (
-              <ItemDetailSheet
-                key={item.id}
-                item={item}
-                setSelectedItem={setSelectedItem}
-              />
-            ))
+            <div className="mt-10 overflow-y-scroll h-full space-y-4 pb-20">
+              {filteredItems.map((item) => (
+                <ItemDetailSheet
+                  key={item.id}
+                  item={item}
+                  themeColor={themeColor}
+                  setSelectedItem={setSelectedItem}
+                />
+              ))}
+            </div>
           )}
         </div>
       </div>
     </>
   );
 };
-
 export default App;
--- src/components/ItemDetailSheet.tsx
+++ src/components/ItemDetailSheet.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Sheet,
   SheetTrigger,
@@ -11,15 +11,27 @@ import { Item } from "../lib/types";
 interface ItemDetailSheetProps {
   item: Item;
   setSelectedItem: (item: Item | null) => void;
+  themeColor: string;
 }
 
-const ItemDetailSheet: React.FC<ItemDetailSheetProps> = ({ item, setSelectedItem }) => {
+const ItemDetailSheet: React.FC<ItemDetailSheetProps> = ({
+  item,
+  setSelectedItem,
+  themeColor,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setSelectedItem(null); // Clear selected item on close
+  };
+
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild>
         <div
-          onClick={() => setSelectedItem(item)} // Set the selected item on click
-          className="flex p-5  rounded-lg hover:shadow-lg mx-2 cursor-pointer bg-white "
+          onClick={() => setIsOpen(true)} // Open the sheet on click
+          className="flex p-5 rounded-lg hover:shadow-lg mx-2 cursor-pointer bg-white"
         >
           <div className="size-14 rounded object-contain overflow-hidden border flex items-center justify-center mr-5">
             <img src={item.image} alt={item.name} className="w-full" />
@@ -30,15 +42,45 @@ const ItemDetailSheet: React.FC<ItemDetailSheetProps> = ({ item, setSelectedItem
           </div>
         </div>
       </SheetTrigger>
-      <SheetContent side="bottom">
-        <div className="p-6 space-y-4">
-          <div className="size-50 flex items-center justify-center object-contain ">
-            <img src={item.image} alt={item.name} className="w-full  object-cover rounded-lg" />
+      <SheetContent className="mx-8" side="bottom">
+        <div className="relative p-4 space-y-4">
+          <div className="size-50 flex items-center justify-center object-contain">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full object-cover rounded-lg"
+            />
           </div>
           <div className="text-center">
-            <SheetTitle className="text-2xl font-bold">{item.name}</SheetTitle>
-            <p className="text-xl text-green-600 mt-2">{item.price} IQD</p>
-            <SheetDescription className="text-sm text-gray-500 mt-4">{item.description || 'No details available.'}</SheetDescription>
+            <SheetTitle className="text-3xl font-semibold">
+              {item.name}
+            </SheetTitle>
+            <p className="text-xl text-white mt-2">
+              <span
+                style={{
+                  // backgroundColor: themeColor,
+                  color: themeColor,
+                }}
+                className=""
+              >
+                {item.price} IQD
+              </span>
+            </p>
+            <SheetDescription className="text-sm text-gray-500 mt-4 ">
+              {item.description || "No details available."}
+            </SheetDescription>
+          </div>
+          {/* Close Button */}
+          <div className="flex justify-center mt-6">
+            <button
+              style={{
+                backgroundColor: themeColor,
+              }}
+              onClick={handleClose}
+              className="text-white py-3 px-14 rounded-full text-lg font-semibold transition"
+            >
+              Close
+            </button>
           </div>
         </div>
       </SheetContent>
